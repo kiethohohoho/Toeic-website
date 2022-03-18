f522aa8fb9d822f420c4fd753dcea801bd8f8b17
Lee Gusion <1951120099@sv.ut.edu.vn>
2022-03-18 22:14:51 +0700

    redeploy

--- src/features/MainPage/HomePage.jsx
+++ src/features/MainPage/HomePage.jsx
@@ -1,8 +1,13 @@
 import LayoutMainPage from "components/LayoutMainPage";
+import Course from "features/Course";
 import React from "react";
 
 const HomePage = () => {
-  return <LayoutMainPage></LayoutMainPage>;
+  return (
+    <LayoutMainPage>
+      <Course />
+    </LayoutMainPage>
+  );
 };
 
 export default HomePage;
